a4d282e34929db0eb59ce25b61d3575abe8781e1
magicd-ops <magictest2020@gmail.com>
2020-12-12 11:05:07 +0330

    move db file to npmjs package and install in package.json

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-let db = require('./db');
+let DB = require('magic-mongodb');
 let {v4: uuidv4} = require('uuid');
 let app = require('express')();
+
+let url = false;//'mongodb://localhost:27017';
+let options = false;//{ useUnifiedTopology: true };
+// name of db
+let dbName = 'life';
+// name of tables
+let collections = [
+    'user'
+];
+
+let db = new DB({dbName, collections});
 
 app.get('/', (req, res) => {
     let code = uuidv4(); // unique code for every request
