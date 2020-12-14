866ec2f8fb0bf845531e7d7c2dd571816221d4af
Majid <dat.omega.sherly@gmail.com>
2020-12-15 02:42:26 +0330

    some updates for check getData , updateData , createData , deleteData

--- index.js
+++ index.js
@@ -11,13 +11,34 @@ let collections = [
     'user'
 ];
 
-let db = new DB({dbName, collections});
+let db = DB({dbName, collections});
 
 app.get('/', (req, res) => {
     let code = uuidv4(); // unique code for every request
-    db.getEv({code ,collectionName: 'user', query: {address: 'test'}})
-    db.on(code, (data) => {
+    db.getData('user'); //, {query: {address: 'test'}}
+    db.response((data) => {
         data ? res.send(data) : res.status(404).send('error in fetch data');
+    });
+});
+
+app.get('/create', (req, res) => {
+    db.createData('user', { username: "test", firstname: "test", lastName: "test" });
+    db.response((result) => {
+        result ? res.send(result) : res.status(404).send('error in create data');
+    });
+});
+
+app.get('/update', (req, res) => {
+    db.updateData('user', {firstname: 'test222'}, { query: { username: 'test' }/** , type='many' */ });
+    db.response((result) => {
+        result ? res.send(result) : res.status(404).send('error in update data');
+    });
+});
+
+app.get('/delete', (req, res) => {
+    db.deleteData('user', {query: { firstname: 'test222' }});
+    db.response((result) => {
+        result ? res.send(result) : res.status(404).send('error in update data');
     });
 });
 
